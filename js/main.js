70e63da83f3ed2a353c889795dba7c68559c75d2
Locs85 <alexbaroyan@gmail.com>
2020-07-24 11:45:44 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import $ from '../node_modules/jquery/dist/jquery.min.js';
+import popper from '../node_modules/popper.js/dist/popper.min.js';
+import bootstrap from '../node_modules/bootstrap/dist/js/bootstrap.min.js';
